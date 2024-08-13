8d9f13ea6a6dcc1cf5ff35fe8838d8d620a3600c
LiamDCreator <Liamduijn@gmail.com>
2024-08-13 13:54:05 +0200

    player collision enemy

--- Assets/scripts/playerScript.cs
+++ Assets/scripts/playerScript.cs
@@ -35,4 +35,10 @@ public class playerScript : MonoBehaviour
             
            
         }}
+         void OnCollisionEnter2D(Collision2D collision){
+ 
+    if (collision.gameObject.CompareTag("Enemy")) 
+        { 
+         Destroy(gameObject);
+    }}
 }
