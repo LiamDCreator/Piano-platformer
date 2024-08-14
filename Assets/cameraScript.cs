33470f43bff5bab673652817df578b4dcb9f18f0
LiamDCreator <Liamduijn@gmail.com>
2024-08-14 12:44:40 +0200

    camera follows player

--- Assets/cameraScript.cs
+++ Assets/cameraScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class cameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+  public Transform player;
+ public Vector3 offset;
+ 
+ void Update () 
+ {
+     transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+ }
+ }
